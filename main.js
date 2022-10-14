6ea4cfe69d320df060cc3d841356675547b06e12
Angel Lars <lars50593@gmail.com>
2022-10-14 11:57:37 -0600

    Fusión del menú en mobile

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu')
+const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu')
 
 menuEmail.addEventListener('click', toggleDesktop);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktop(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
